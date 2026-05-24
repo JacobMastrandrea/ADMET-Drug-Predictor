132533794cd2b81f52a86934180cd13c3f5ad848
Jacob <jmastra5@uwo.ca>
2026-05-24 11:15:56 -0400

    Essentially wrapped up linear regression/lasso baseline test, with mixed results. Next step is more advanced modelling, which should capture more non-linear relationships

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from src.preprocessing import preprocess
 from sklearn.model_selection import cross_val_score, cross_val_predict
 
-from sklearn.linear_model import Lasso
-from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LassoCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 
 def train_linear(df, target):
@@ -36,9 +36,40 @@ def test_linear(df, target, model):
     """
     Testing Linear Regression Modles.
     """
-    X =df.drop(columns=[target]).select_dtypes(include=['float64', 'int64'])
+    X =df.drop(columns=[target]).select_dtypes(include=['float64', 'int64']) #drop the target columns and only keep int64 and float64 dtypes
 
-    y_pred = model.predict(X)
+    y_pred = model.predict(X) #predict the targets using the features (X)
 
     return y_pred
 
+
+def train_lasso(df, target):
+    """
+    Training Lasso Lasso Models
+    """
+    #-- Define features and target
+    X = df.drop(columns=[target]).select_dtypes(include=['float64', 'int64'])
+    y = df[target]
+
+    #-- A Pipeline is used here to ensure the scaler is fit only on training folds, preventing data leakage during cross val.
+    pipeline = Pipeline([
+        ('scaler',StandardScaler()),
+        ('lasso',LassoCV(cv=10))
+    ])
+    
+    pipeline.fit(X,y)
+
+    scores = cross_val_score(pipeline, X, y, cv=10, scoring='r2')
+    y_pred_cv = cross_val_predict(pipeline, X, y, cv=10)
+     
+    return pipeline, scores, X, y, y_pred_cv
+
+def test_lasso(df, target, model):
+    """
+    Testing Lasso Modles.
+    """
+    X =df.drop(columns=[target]).select_dtypes(include=['float64', 'int64']) #drop the target columns and only keep int64 and float64 dtypes
+
+    y_pred = model.predict(X) #predict the targets using the features (X)
+
+    return y_pred
